334f77bce92edeafc879a9e903dc2636143ec636
Abdullah Al Kafi <kafikafi1922@gmail.com>
2024-05-21 03:30:08 +0600

    Server connected to database successfully

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import connectDB from "./DB";
+import app from "./app";
+import config from "./app/config";
+
+connectDB()
+  .then(() => {
+    app.listen(config.port || 3000, () => {
+      console.log(`Example app listening on port ${config.port || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDB connection failed ", error);
+  });
